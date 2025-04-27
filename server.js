e06af9d46e3971fd2754b0248e927b7424cf46ae
Asma Arain <greatasma@yahoo.com>
2025-04-28 00:38:50 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+});
